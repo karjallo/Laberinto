ea6557b56a2c907a06d635bfb54db0295058b3ab
Samuel Karjallo <karjallo@disroot.org>
2026-04-09 16:16:32 -0300

    definimos mayor dimension del laberinto y optimizamos funcion imprimir_tablero

--- laberinto.c
+++ laberinto.c
@@ -1,16 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-void imprimir_laberinto(int tamano, int matriz[][tamano]);
+#define MAX 101
+// 0 es muro, 1 es espacio vacio,  2 es camino correcto, 3 es entrada, 4 salida
+void imprimir_laberinto(int tamano, int matriz[][tamano]){
+
+    for (int i = 0; i < tamano; i++){
+        for (int j = 0; j < tamano; j++){
+            switch (matriz[i][j])
+            {
+            case 0:
+                printf("#");
+                break;
+            case 1:
+                printf(" ");
+                break;
+            case 2:
+                printf("*");
+                break;
+            case 3:
+                printf("O");
+                break;
+            case 4:
+                printf("X");
+                break;
+            default:
+                printf("&");
+                break;
+        }
+        printf("\n");
+
+    }
+}
 
 int main(int argc, char *argv[]) {
     
-    int dimension = 10;
+    int laberinto[MAX][MAX];
+    int dimension = 21;
+    laberinto[dimension][dimension] = {0};
+    laberinto[1][0] = 
+
     if (argc > 1){
         dimension = atoi(argv[1]);
+        if (dimension > MAX || dimension <= 0 || dimension % 2 != 0){
+            printf("opcion invalida %s \n Ingrese un numero impar hasta %d", argv[1], MAX);
+        }
     }
-
-    int laberinto[dimension][dimension];
 
     for (int i = 0; i < dimension; i++){
         for (int j = 0; j < dimension; j++){
@@ -20,18 +56,5 @@ int main(int argc, char *argv[]) {
 
     imprimir_laberinto(dimension, laberinto);
     return 0;
-}
-
-void imprimir_laberinto(int tamano, int matriz[][tamano]){
-
-    for (int i = 0; i < tamano; i++){
-        for (int j = 0; j < tamano; j++){
-            if (matriz[i][j] == 0){
-                printf("# ");
-            }
-        }
-        printf("\n");
-        
-    }
 }
 
